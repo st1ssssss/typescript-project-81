033fe7c0d96b1b4f9a3e4601aaf58702d90a87fe
Shokurov Kirill <kirill.shokurov@metran.ru>
2025-03-26 17:00:00 +0300

    update index ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,2 @@
-import Tag from './tags/Tag.js'
 import HexletCode from '../src/HexletCode/HexletCode.js'
 export default HexletCode
-const br = new Tag('br')
-
-const input = new Tag('input', { type: 'radio' })
-
-const img = new Tag('img', { alt: 'someImg', class: 'someImg', atr: 'sdsdd' })
-
-const div = new Tag('div', {}, 'ghbdtn')
-
-console.log(br.toString())
-
-console.log(input.toString())
-
-console.log(div.toString())
