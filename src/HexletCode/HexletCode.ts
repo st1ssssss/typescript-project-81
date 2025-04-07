9a968694928a83ba0acd5de0d948e2f423fb3bfc
Shokurov Kirill <kirill.shokurov@metran.ru>
2025-04-07 11:56:16 +0300

    removed render logic from Form class

--- src/HexletCode/HexletCode.ts
+++ src/HexletCode/HexletCode.ts
@@ -9,34 +9,56 @@ type Methods = 'get' | 'post'
 interface InputOtions {
   as?: 'input' | 'textarea'
 }
+interface IFieldTemplates {
+  opt?: InputOtions & Record<string, string | number>
+  name?: string
+  value?: string
+}
 
 class FormBuilder {
+  public static builder(template: IFieldTemplates[]) {
+    const form = template.map((el) => {
+      if (el.opt !== undefined) {
+        const filteredInputOpt = Object.fromEntries(Object.entries(el.opt).filter((el) => {
+          if (el[0] !== 'as') {
+            return true
+          }
+        }))
+        if (el.opt?.as === 'textarea') {
+          const formString = new Textarea ({ ...filteredInputOpt, name: el.name! }, el.value, true).toString()
+          return formString
+        }
+        else if (el.opt?.type === 'submit') {
+          const formString = new Input({ type: 'submit', value: el.opt.value as string }).toString()
+          return formString
+        }
+        else {
+          const formString = new Input ({ name: el.name!, type: 'text', value: el.value!, ...filteredInputOpt }, true).toString()
+          return formString
+        }
+      }
+      else {
+        const formString = new Input ({ name: el.name!, type: 'text', value: el.value! }, true).toString()
+        return formString
+      }
+    })
+    return form.join('')
+  }
+}
+class Form {
   constructor(private template: ITemplate, public options: { method: 'post' | 'get', action: string }) {}
 
-  fields: string[] = []
+  fieldsTemplates: IFieldTemplates[] = []
 
   input(templateKeyName: string, inputOpt?: InputOtions & Record<string, string | number>) {
     const value = this.template[templateKeyName]
 
     if (value !== undefined) {
       if (inputOpt !== undefined) {
-        const filteredInputOpt = Object.fromEntries(Object.entries(inputOpt).filter((el) => {
-          if (el[0] !== 'as') {
-            return true
-          }
-        }))
-        if (inputOpt.as === 'textarea') {
-          const formString = new Textarea ({ ...filteredInputOpt, name: templateKeyName }, value, true).toString()
-          this.fields.push(formString)
-        }
-        else {
-          const formString = new Input ({ name: templateKeyName, type: 'text', value: value, ...filteredInputOpt }, true).toString()
-          this.fields.push(formString)
-        }
+        this.fieldsTemplates.push({ opt: inputOpt, name: templateKeyName, value: value })
       }
       else {
-        const formString = new Input ({ name: templateKeyName, type: 'text', value: value }, true).toString()
-        this.fields.push(formString)
+        this.fieldsTemplates.push({ name: templateKeyName, value: value })
       }
     }
     else {
@@ -45,20 +67,19 @@ class FormBuilder {
   }
 
   submit(placeholder = 'Save') {
-    const formString = new Input({ type: 'submit', value: placeholder }).toString()
-    this.fields.push(formString)
+    this.fieldsTemplates.push({ opt: { type: 'submit', value: placeholder } })
   }
 
   get getFields() {
-    return this.fields
+    return this.fieldsTemplates
   }
 }
 
 export default class HexletCode {
-  public static formFor(template: ITemplate, methods: { url?: string, method?: Methods }, callback: (v: FormBuilder) => void) {
-    const builder = new FormBuilder(template, { method: methods.method ?? 'post', action: methods.url ?? '#' })
-    callback(builder)
-    const formStrings = builder.getFields.join('')
-    return new Tag('form', builder.options, formStrings).toString()
+  public static formFor(template: ITemplate, methods: { url?: string, method?: Methods }, callback: (v: Form) => void) {
+    const form = new Form(template, { method: methods.method ?? 'post', action: methods.url ?? '#' })
+    callback(form)
+    const formStrings = FormBuilder.builder(form.getFields)
+    return new Tag('form', form.options, formStrings).toString()
   }
 }
